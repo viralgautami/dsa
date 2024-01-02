dd103d6493985525b016ec3d435e20353f0f8cb7
viralgautami <d22dcs141@charusat.edu.in>
2024-01-02 23:23:16 +0530

    binary_search

--- array/binary_search.java
+++ array/binary_search.java
@@ -3,18 +3,26 @@ package array;
 public class binary_search {
     public static int binarySearch(int numbers[], int key){
         int start=0, end=numbers.length-1;
-
+        System.out.println(end);
         while(start <= end){
-            int mid = (start+end)/1;
-            
+            int mid = (start+end)/2;
             if(numbers[mid]==key){
                 return mid;
             }
+            if(numbers[mid]<key){
+                start = mid+1;
+            }
+            else{
+                end=mid-1;
+            }
 
         }
+        return -1;
     }
 
     public static void main(String[] args) {
-        
+        int numbers[] = {1,2,3,6,7,9};
+        int key=9;
+        binarySearch(numbers, key);
     }    
 }
